1ebb02446a8a5efaabaa5122b205c9a57d492e99
RASEL KIBRIA <rasel5jkibria@gmail.com>
2020-09-03 13:04:15 +0600

    Back to High School Physics

---
+++ 10071.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+
+    int v, t;
+    while(scanf("%d %d", &v, &t)!=EOF)
+    {
+        printf("%d\n", v*t*2);
+    }
+    return 0;
+}
