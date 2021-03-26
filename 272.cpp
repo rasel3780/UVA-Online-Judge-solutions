8ad0fb09aee54304acff4ba435c95037b5d44337
rasel3780 <rasel5jkibria@gmail.com>
2021-03-27 03:03:07 +0600

    TEX Quotes

---
+++ 272.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define FILE freopen("in.txt", "r", stdin), freopen("out.txt", "w", stdout)
+using namespace std;
+
+void sol(){
+    string s;
+    int tp =0;
+    while(getline(cin, s)){
+        int len = s.size();
+        for(int i=0; i<len; i++){
+            if(s[i]=='"' && tp==0){
+                printf("``");
+                //i++;
+                tp=1;
+            }
+            else if(s[i]=='"' && tp==1){
+                printf("''");
+                //i++;
+                tp=0;
+            }
+            else{
+                printf("%c", s[i]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    FILE;
+
+    sol();
+}
